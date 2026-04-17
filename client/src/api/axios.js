c830b557a156e00312cdfd431c8b5f7aa51cccd3
honeya5 <238w1a12a5@vrsec.ac.in>
2026-04-17 16:30:30 +0530

    deploy the project

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API = axios.create({
-    baseURL:"https://mern-e-commerce-6jyq.onrender.com"
+    baseURL:"https://mern-e-commerce-6jyq.onrender.com/api"
 })
 API.interceptors.request.use((req)=>{
     const token=localStorage.getItem("token")
